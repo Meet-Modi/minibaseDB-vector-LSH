c97e4ff17b442f6651af17906ac39ed386aed798
dbasina <dbasina@asu.edu>
2025-03-03 12:19:33 -0700

    Setup steps in test 2 for how to setup basic functinality needed for testing. This is how the abstractions will interact with each other.

--- javaminibase/src/tests/VectorTest.java
+++ javaminibase/src/tests/VectorTest.java
@@ -8,6 +8,7 @@ import diskmgr.*;
 import heap.*;
 import iterator.*;
 import index.*;
+import LSHFIndex.*;
 
 import java.util.Random;
 
@@ -326,6 +327,25 @@ class VectorDriver extends TestDriver implements GlobalConst
     @Override
     protected boolean test2()
     {
+        // Step 1: Generate input text file to read 10,000 random vectors from.
+
+        // Step 2: Specify the number of hash functions per layer (n).
+
+        // Step 3: Specify the number of layers (L)
+
+        // Step 4: instantiate the n hash functions L times. ( h_1,1 h_1,2 h_1,3 h_1,4 h_1,5,
+        //                                                     h_2,1 h_2,2 h_2,3 h_2,4 h_2,5,
+        //                                                     h_3,1 h_3,2 h_3,3 h_3,4 h_3,5 )
+
+        // Step 5: The hasher is an abstraction that generates the hash values above. ^^^^^^^^^^
+
+        // Step 6: using index file, access the corresponding 3 heap file with names   ( h_11 : h_12 : h_13 : h_14 : h_15,
+        //                                                                               h_21 : h_22 : h_23 : h_24 : h_25,
+        //                                                                               h_31 : h_32 : h_33 : h_34 : h_35 )
+
+        // Step 7: compute union of all tuples from all 3 files into a temp heapfile
+        // Step 8: run Sort() on the temp heapfile k times for k-NN search. if k = 0, return all results
+        // Step 9: see the alternative query pattern for the all elements in range k and implement its functionality.
         return true;
     }
 
