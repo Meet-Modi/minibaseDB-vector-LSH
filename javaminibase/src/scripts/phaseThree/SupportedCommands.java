c9419d61642b2189adfaeeec3cbf809d5437243b
Vikram <vkumare1@asu.edu>
2025-05-02 11:45:52 -0700

    Add sh launch script

--- javaminibase/src/scripts/phaseThree/SupportedCommands.java
+++ javaminibase/src/scripts/phaseThree/SupportedCommands.java
@@ -4,7 +4,7 @@ public enum SupportedCommands {
     OPEN_DB ("open_database", "open_database <db name>"),
     CLOSE_DB ("close_database", "close_database"),
     BATCH_CREATE("batchcreate", "batchcreate <data file name> <rel name>"),
-    CREATE_INDEX("create_index", "createindex <rel name> <column id> <L> <H>"),
+    CREATE_INDEX("createindex", "createindex <rel name> <column id> <L> <H>"),
     BATCH_INSERT("batchinsert", "batchinsert <data file name> <rel name>"),
     BATCH_DELETE("batchdelete", "batchdelete <data file name> <rel name>"),
     QUERY("query", "query <rel1 name> <rel2 name> <qsname> <numbuf>");
